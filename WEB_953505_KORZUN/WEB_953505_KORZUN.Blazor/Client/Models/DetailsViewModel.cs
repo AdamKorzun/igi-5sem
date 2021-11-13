8a6a0844c7b2259e4ea615d7f4cbeb99cc00bfc4
AdamKorzun <a.korzun21@gmail.com>
2021-11-13 21:38:51 +0300

    lab08

---
+++ WEB_953505_KORZUN/WEB_953505_KORZUN.Blazor/Client/Models/DetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace WEB_953505_KORZUN.Blazor.Client.Models
+{
+    public class DetailsViewModel
+    {
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; } 
+        [JsonPropertyName("description")]
+        public string Description { get; set; } 
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+        [JsonPropertyName("image")]
+        public string Image { get; set; } 
+    }
+}
---
+++ WEB_953505_KORZUN/WEB_953505_KORZUN.Blazor/Client/Models/ListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.Json.Serialization;
+namespace WEB_953505_KORZUN.Blazor.Client.Models
+{
+    public class ListViewModel
+    {
+        [JsonPropertyName("productId")]
+        public int ProductId { get; set; } 
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; }
+    }
+}
